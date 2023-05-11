4c4e421031c999c8ed4111fd2719877d52508b04
minji <kmj99a@gmail.com>
2023-05-11 17:59:03 +0900

    [Update] JWT 필터 적용

--- src/main/java/xyz/wavey/apigateway/FilterConfig.java
+++ src/main/java/xyz/wavey/apigateway/FilterConfig.java
@@ -13,9 +13,9 @@ public class FilterConfig {
             , AuthorizationHeaderFilter authorizationHeaderFilter) {
         return builder.routes()
                 // rental-service
-                .route(p -> p.path("/rental/**", "/insurance/**").uri("lb://RENTAL-SERVICE"))
+                .route(p -> p.path("/rental/**", "/insurance/**").filters(f -> f.filters(authorizationHeaderFilter.apply(config -> {}))).uri("lb://RENTAL-SERVICE"))
                 // user-service
-                .route(p -> p.path("/auth/**").uri("lb://USER-SERVICE"))
+                .route(p -> p.path("/auth/**","/user/**").uri("lb://USER-SERVICE"))
 //                .route(p -> p.path("/auth/**")
 //                        .filters(f -> f.filters(authorizationHeaderFilter.apply(config -> {})))
 //                        .uri("lb://USER-SERVICE"))
